b07d6ea7f48eb0dd95453ebe6d8c93f313cfcd8b
hewenjin <hewenjin94@outlook.com>
2023-07-30 16:55:31 +0800

    update local/bin

--- .local/bin/record-script.sh
+++ .local/bin/record-script.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
 
-cd /home/end/Videos
+cd /home/$USER/Videos
 if [[ "$(pidof wf-recorder)" == "" ]]; then
     notify-send "wf-recorder" "Starting recording" -a 'wf-recorder'
     wf-recorder -f './recording_'"$(date '+%Y_%m_%_d..%H.%M')"'.mp4' -t --geometry "$(slurp)" 
