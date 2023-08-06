9e337a46822e61100b2e8cd3eae782497710ac3d
hewenjin <hewenjin94@outlook.com>
2023-08-06 16:17:00 +0800

    add config init script

---
+++ .local/bin/cfg-init.sh
@@ -0,0 +1,14 @@
+git clone --bare https://github.com/hewenjin/config.git $HOME/.cfg
+function cfg {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+cfg checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} {}.bak
+fi;
+cfg checkout
+cfg config status.showUntrackedFiles no
