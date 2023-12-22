a140334cd34205c8d5d412c8379a7dec58a77165
hewenjin <hewenjin94@outlook.com>
2023-12-22 18:10:16 +0800

    disable google-java-format

--- .config/nvim/lua/plugin/lspconfig.lua
+++ .config/nvim/lua/plugin/lspconfig.lua
@@ -192,14 +192,14 @@ return {
 
       -- register any number of sources simultaneously
       local sources = {
-        null_ls.builtins.formatting.google_java_format,
+        -- null_ls.builtins.formatting.google_java_format,
         null_ls.builtins.diagnostics.cspell.with({
           diagnostics_postprocess = function(diagnostic)
             diagnostic.severity = vim.diagnostic.severity["INFO"]
           end,
         }),
         null_ls.builtins.code_actions.cspell,
-        null_ls.builtins.code_actions.gitsigns
+        null_ls.builtins.code_actions.gitsigns,
       }
       null_ls.setup({ sources = sources })
     end
