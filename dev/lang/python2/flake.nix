87ba08c796b7dfb8e7481cb4ee45608c88685ac1
wenjin <hewenjin94@outlook.com>
2024-02-18 16:55:53 +0800

    add dev shell template

---
+++ dev/lang/python2/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { 
+          inherit system;
+          config = {
+            permittedInsecurePackages = [
+              "python-2.7.18.7"
+            ];
+          };
+        };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ python2 virtualenv ];
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      });
+    };
+}
