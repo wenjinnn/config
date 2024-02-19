880ab82c09afe16c91fc19f3c8ed21eed7d38dbf
wenjin <hewenjin94@outlook.com>
2024-02-19 15:12:01 +0800

    reuse git for nvim diffformat

--- xdg/config/nvim/lua/plugin/editing.lua
+++ xdg/config/nvim/lua/plugin/editing.lua
@@ -5,26 +5,44 @@ return {
     config = function()
       require('conform').setup()
       local diff_format = function()
-        local last_line = vim.fn.line('$')
-        local start_line = vim.fn.line("'[");
-        local end_line = vim.fn.line("']")
-        if start_line == 1 and endline == last_line then
+        if not vim.fn.has('git') then
           return
         end
-        require('conform').format {
-          lsp_fallback = true,
-          timeout_ms = 500,
-          range = {
-            start = { start_line, 0},
-            ['end'] = { end_line, vim.fn.col('$') }
-          },
-        }
+        local filename = vim.fn.expand('%:p');
+        local lines = vim.fn.system('git diff --unified=0 ' .. filename):gmatch('[^\n\r]+')
+        local ranges = {}
+        for line in lines do
+          if line:find('^@@') then
+            local line_nums = line:match('%+.- ')
+            if line_nums:find(',') then
+              local _, _, first, second = line_nums:find('(%d+),(%d+)')
+              table.insert(ranges, {
+                start = { tonumber(first), 0 },
+                ['end'] = { tonumber(first) + tonumber(second), 0 },
+              })
+            else
+              local first = tonumber(line_nums:match('%d+'))
+              table.insert(ranges, {
+                start = { first, 0 },
+                ['end'] = { first + 1, 0 },
+              })
+            end
+          end
+        end
+        local format = require('conform').format
+        for _, range in pairs(ranges) do
+          format {
+            lsp_fallback = true,
+            timeout_ms = 500,
+            range = range,
+          }
+        end
       end
       vim.api.nvim_create_user_command('DiffFormat',
         diff_format,
         { desc = 'Format changed lines' }
       )
-      vim.api.nvim_create_autocmd('InsertLeave', {
+      vim.api.nvim_create_autocmd('BufWritePre', {
         pattern = '*',
         callback = diff_format,
         desc = 'Auto Format changed lines'
