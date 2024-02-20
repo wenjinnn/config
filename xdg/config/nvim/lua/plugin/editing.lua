8ff834a0707afac75a3e2a3b6e459557bb9d06c7
wenjin <hewenjin94@outlook.com>
2024-02-20 22:05:20 +0800

    remove useless code

--- xdg/config/nvim/lua/plugin/editing.lua
+++ xdg/config/nvim/lua/plugin/editing.lua
@@ -174,30 +174,6 @@ return {
         extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
         -- max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
       },
-      refactor = {
-        highlight_definitions = {
-          enable = false,
-          -- Set to false if you have an `updatetime` of ~100.
-          clear_on_cursor_move = false,
-        },
-        highlight_current_scope = { enable = true },
-        smart_rename = {
-          enable = true,
-          keymaps = {
-            smart_rename = "gR",
-          },
-        },
-        navigation = {
-          enable = true,
-          keymaps = {
-            goto_definition = "gnd",
-            list_definitions = "gnD",
-            list_definitions_toc = "gO",
-            goto_next_usage = "<a-*>",
-            goto_previous_usage = "<a-#>",
-          },
-        },
-      },
       textobjects = {
         select = {
           enable = true,
