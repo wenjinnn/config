15d2eea2b4370fdd8f0683714a9e3b2134432f6d
wenjin <hewenjin94@outlook.com>
2024-02-20 18:07:27 +0800

    edit treesitter config

--- xdg/config/nvim/lua/plugin/editing.lua
+++ xdg/config/nvim/lua/plugin/editing.lua
@@ -130,6 +130,7 @@ return {
     },
     build = ':TSUpdate',
     event = { 'BufReadPre' },
+    cond = not vim.g.vscode,
     init = function(plugin)
       -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
       -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
@@ -149,14 +150,14 @@ return {
       ensure_installed = 'all',    -- one of "all", "maintained" (parsers with maintainers), or a list of languages
       ignore_install = {},         -- List of parsers to ignore installing
       highlight = {
-        enable = not vim.g.vscode, -- false will disable the whole extension
+        enable = true, -- false will disable the whole extension
         disable = {}               -- list of language that will be disabled
       },
       autopairs = {
         enable = true
       },
       rainbow = {
-        enable = not vim.g.vscode,
+        enable = true,
         extended_mode = true -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
         -- max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
       },
