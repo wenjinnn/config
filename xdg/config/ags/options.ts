846a4a4df8b834ed0b8acc85e27c2bbd5688183e
wenjin <hewenjin94@outlook.com>
2024-03-10 19:17:41 +0800

    adjust ags hyprland gaps

--- xdg/config/ags/options.ts
+++ xdg/config/ags/options.ts
@@ -206,7 +206,7 @@ const options = mkOptions(OPTIONS, {
     },
 
     hyprland: {
-        gaps: opt(2.4),
+        gaps: opt(1.6),
         inactiveBorder: opt("333333ff"),
         gapsWhenOnly: opt(true),
     },
