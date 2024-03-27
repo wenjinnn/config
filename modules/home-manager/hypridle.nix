83f54e1bd316a48756947ae315045fc6a0cbb37f
wenjin <hewenjin94@outlook.com>
2024-03-27 21:37:03 +0800

    fix: temporary fix wlsunset not work after hyprland dispatch dpms off/on

--- modules/home-manager/hypridle.nix
+++ modules/home-manager/hypridle.nix
@@ -10,11 +10,12 @@
     hyprlock = "${pkgs.hyprlock}/bin/hyprlock";
     hyprctl = "${pkgs.unstable.hyprland}/bin/hyprctl";
     loginctl = "${pkgs.systemd}/bin/loginctl";
+    restartWlsunset = "systemd --user restart wlsunset.service";
   in {
     enable = true;
     lockCmd = "pidof hyprlock || ${hyprlock}";
     beforeSleepCmd = "${hyprctl} dispatch dpms off";
-    afterSleepCmd = "${hyprctl} dispatch dpms on && ${loginctl} lock-session";
+    afterSleepCmd = "${hyprctl} dispatch dpms on && ${loginctl} lock-session && ${restartWlsunset}";
     listeners = [
       {
         timeout = 300;
@@ -23,7 +24,7 @@
       {
         timeout = 360;
         onTimeout = "${hyprctl} dispatch dpms off";
-        onResume = "${hyprctl} dispatch dpms on";
+        onResume = "${hyprctl} dispatch dpms on && ${restartWlsunset}";
       }
     ];
   };
