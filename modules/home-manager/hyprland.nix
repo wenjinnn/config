721031b5d91c8082be43497d8a0e1f0221152dd0
wenjin <hewenjin94@outlook.com>
2024-04-27 09:59:17 +0800

    chore(hyprland): adjust xwaylandvideobridge settings

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -252,10 +252,11 @@
             "nofocus,title:^(.*)(mvi)$"
           ];
           windowrulev2 = [
-            "opacity 0.0 override 0.0 override,class:^(xwaylandvideobridge)$"
+            "opacity 0.0 override,class:^(xwaylandvideobridge)$"
             "noanim,class:^(xwaylandvideobridge)$"
             "nofocus,class:^(xwaylandvideobridge)$"
             "noinitialfocus,class:^(xwaylandvideobridge)$"
+            "maxsize 1 1,class:^(xwaylandvideobridge)$"
           ];
           layerrule = [
             # "blur, powermenu"
