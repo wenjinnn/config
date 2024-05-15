2e7f423b97f218eda5e101d0f5483226387562ba
wenjin <hewenjin94@outlook.com>
2024-05-15 20:44:10 +0800

    feat(neovim): edit lua ls settings

--- xdg/config/nvim/lua/lsp/lua_ls/init.lua
+++ xdg/config/nvim/lua/lsp/lua_ls/init.lua
@@ -1,40 +1,31 @@
 local M = {}
-M.settings = {
-  Lua = {
+M.on_init = function(client)
+  local path = client.workspace_folders[1].name
+  if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+    return
+  end
+
+  client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
     runtime = {
-      -- LuaJIT in the case of Neovim
+      -- Tell the language server which version of Lua you're using
+      -- (most likely LuaJIT in the case of Neovim)
       version = "LuaJIT",
     },
-    format = {
-      enable = true,
-      defaultConfig = {
-        indent_style = "space",
-        indent_size = "2",
-        quote_style = "single",
-        --   align_if_branch = false,
-        -- align_array_table = false,
-        -- align_continuous_assign_statement = false,
-        -- align_continuous_rect_table_field = false,
-        align_call_args = "false",
-        align_function_params = "false",
-        align_continuous_assign_statement = "false",
-        align_continuous_rect_table_field = "false",
-        align_array_table = "true",
-      },
-    },
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = { "vim" },
-    },
+    -- Make the server aware of Neovim runtime files
     workspace = {
       checkThirdParty = false,
-      -- Make the server aware of Neovim runtime files
-      library = vim.env.VIMRUNTIME,
+      library = {
+        vim.env.VIMRUNTIME,
+        -- Depending on the usage, you might want to add additional paths here.
+        -- "${3rd}/luv/library"
+        -- "${3rd}/busted/library",
+      },
+      -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+      -- library = vim.api.nvim_get_runtime_file("", true)
     },
-    -- Do not send telemetry data containing a randomized but unique identifier
-    telemetry = {
-      enable = false,
-    },
-  },
+  })
+end
+M.settings = {
+  Lua = {},
 }
 return M
