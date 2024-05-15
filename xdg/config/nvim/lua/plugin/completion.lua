608c7fabdb87897b71c5fb6d682d0dbc22f90ef5
wenjin <hewenjin94@outlook.com>
2024-05-16 01:55:53 +0800

    feat(neovim): adjust cmp behavior

--- xdg/config/nvim/lua/plugin/completion.lua
+++ xdg/config/nvim/lua/plugin/completion.lua
@@ -56,10 +56,44 @@ return {
     }
     local vim_dadbod_completion_config = { name = "vim-dadbod-completion" }
 
+    local mapping = cmp.mapping.preset.insert({
+      ["<C-n>"] = cmp.mapping({
+        i = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        c = cmp.mapping.select_next_item(),
+      }),
+      ["<C-p>"] = cmp.mapping({
+        i = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        c = cmp.mapping.select_prev_item(),
+      }),
+      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+      ["<C-e>"] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif vim.fn["vsnip#available"](1) == 1 then
+          feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        end
+      end, { "i", "s", "c" }),
+      ["<S-Tab>"] = cmp.mapping(function()
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+          feedkey("<Plug>(vsnip-jump-prev)", "")
+        end
+      end, { "i", "s", "c" }),
+    })
+
     cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,noinsert,preview",
-      },
       window = {
         documentation = {
           max_width = 70,
@@ -73,43 +107,7 @@ return {
           vim.fn["vsnip#anonymous"](args.body)
         end,
       },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping(
-          cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-          { "i", "c" }
-        ),
-        ["<C-p>"] = cmp.mapping(
-          cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-          { "i", "c" }
-        ),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-e>"] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = true })),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif vim.fn["vsnip#available"](1) == 1 then
-            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-            feedkey("<Plug>(vsnip-jump-prev)", "")
-          end
-        end, { "i", "s" }),
-      }),
+      mapping = mapping,
       sources = {
         cmp_vsnip_source_config,
         { name = "nvim_lsp" },
@@ -175,35 +173,7 @@ return {
     })
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(":", {
-      -- we want manually trigger completion in cmdline, like vim native behavior
-      completion = {
-        autocomplete = false,
-      },
-      mapping = cmp.mapping.preset.cmdline({
-        -- Your configuration here.
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm(),
-        ["<Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            cmp.complete()
-          end
-        end, { "i", "c" }),
-        ["<S-Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            cmp.complete()
-          end
-        end),
-      }),
+      mapping = mapping,
       sources = cmp.config.sources({
         { name = "path" },
       }, {
@@ -211,37 +181,7 @@ return {
       }),
     })
     cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline({
-        -- Your configuration here.
-        ["<C-n>"] = cmp.mapping(
-          cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          { "i", "c" }
-        ),
-        ["<C-p>"] = cmp.mapping(
-          cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          { "i", "c" }
-        ),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm(),
-        ["<Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_next_item()
-          else
-            cmp.complete()
-          end
-        end, { "i", "c" }),
-        ["<S-Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item()
-          else
-            cmp.complete()
-          end
-        end, { "i", "c" }),
-      }),
+      mapping = mapping,
       sources = cmp.config.sources({
         { name = "buffer" },
       }),
