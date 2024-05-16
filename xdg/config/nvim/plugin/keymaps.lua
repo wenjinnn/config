8e6e35ecccf944df1a2baa04c51d28761c0d6391
wenjin <hewenjin94@outlook.com>
2024-05-16 11:43:58 +0800

    refactor(neovim): rewriter keymap option to lua

---
+++ xdg/config/nvim/plugin/keymaps.lua
@@ -0,0 +1,68 @@
+local map = vim.keymap.set
+map("n", "[t", "<cmd>tabp<cr>", { silent = true, desc = "Previous Tab" })
+map("n", "]t", "<cmd>tabn<cr>", { silent = true, desc = "Next Tab" })
+map(
+  "n",
+  "<leader>S",
+  "<cmd>windo set scrollbind!<CR>",
+  { silent = true, desc = "Scroll All Buffer" }
+)
+map("n", "<leader>X", "<cmd>only<CR>", { silent = true, desc = "Only" })
+map("n", "<leader><leader>X", "<c-^>", { silent = true, desc = "Previous Buffer" })
+
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map(
+  { "n", "x" },
+  "<Down>",
+  "v:count == 0 ? 'gj' : 'j'",
+  { desc = "Down", expr = true, silent = true }
+)
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- Add undo break-points
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", ";", ";<c-g>u")
+
+--keywordprg
+map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
+
+-- lazy
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+map("n", "<leader>a", "<cmd>lopen<cr>", { desc = "Location List" })
+map("n", "<leader>q", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+map("n", "<leader>C", "<cmd>!catgs<cr>", { desc = "Ctags" })
+
+if vim.g.vscode then
+  local action = function(action, opts)
+    return function()
+      require("vscode-neovim").action(action, opts)
+    end
+  end
+  map("n", "K", action("editor.action.showHover"))
+  map("n", "gd", action("editor.action.peekDefinition"))
+  map("n", "gD", action("editor.action.peekDeclaration"))
+  map("n", "gh", action("editor.action.showDefinitionPreviewHover"))
+  map("n", "gi", action("editor.action.goToImplementation"))
+  map("n", "gI", action("editor.showIncomingCalls"))
+  map("n", "gO", action("editor.showOutgoingCalls"))
+  map("n", "gr", action("editor.action.goToReferences"))
+  map("v", "<leader>tt", action("translates.translates", { args = { 1 } }))
+  map("n", "<leader>fe", action("workbench.view.explorer"))
+  map("n", "<leader>ff", action("workbench.action.quickOpen"))
+  map("n", "<leader>fe", action("workbench.view.explorer"))
+  map("n", "<leader>ff", action("workbench.view.search"))
+  map("n", "<leader>fg", action("workbench.view.search"))
+  map("n", "<leader>fb", action("workbench.action.quickOpenPreviousRecentlyUsedEditor"))
+  map("n", "[b", action("workbench.action.quickOpenPreviousRecentlyUsedEditor"))
+  map("n", "]b", action("workbench.action.quickOpenLeastRecentlyUsedEditor"))
+  map("n", "<c-\\><c-\\>", action("workbench.action.terminal.toggleTerminal"))
+  map("n", "<leader>mm", action("editor.action.formatDocument"))
+  map("v", "<leader>mm", action("editor.action.formatSelection"))
+  map({ "n", "v" }, "<leader>ca", action("editor.action.quickFix"))
+  map("n", "<leader>x", action("workbench.action.closeActiveEditor"))
+end
---
+++ xdg/config/nvim/plugin/options.lua
@@ -0,0 +1,98 @@
+local opt = vim.opt
+
+opt.errorbells = false
+opt.breakindent = true
+opt.breakindent = true
+opt.signcolumn = "yes"
+opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m"
+opt.formatoptions = "jcroqlnt"
+if not vim.env.SSH_TTY then
+  opt.clipboard = "unnamedplus" -- Sync with system clipboard
+end
+opt.inccommand = "nosplit"
+opt.cmdheight = 1
+opt.showcmd = true
+if vim.g.started_by_firenvim ~= nil then
+  opt.laststatus = 0
+else
+  opt.laststatus = 2
+end
+opt.number = true
+opt.cursorline = true
+opt.virtualedit = { "block", "onemore" }
+-- ignore filetype when file search
+opt.wildignore:append({
+  "*/tmp/*",
+  "*.so",
+  "*.swp",
+  "*.png",
+  "*.jpg",
+  "*.jpeg",
+  "*.gif",
+  "*.class",
+  "*.pyc",
+  "*.pyd",
+})
+-- code indent
+opt.cindent = true
+opt.cinoptions = { "g0", ":0", "N-s", "(0" }
+opt.smartindent = true
+-- tab & space
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.shiftround = true
+opt.softtabstop = 4
+opt.sidescroll = 10
+
+opt.list = true
+opt.listchars = {
+  tab = ">-",
+  precedes = "«",
+  extends = "»",
+  trail = "·",
+  lead = "·",
+  eol = "↴",
+  nbsp = "␣",
+}
+if vim.bo.buftype == "terminal" then
+  opt.listchars:remove({ "eol" })
+end
+
+opt.undofile = true
+opt.undodir = vim.fn.stdpath("data") .. "/undo"
+opt.wrap = true
+opt.relativenumber = true
+opt.mouse = "a"
+
+-- search
+opt.ignorecase = true
+
+opt.autowrite = true
+opt.confirm = true
+opt.updatetime = 500
+opt.termencoding = "utf-8"
+opt.fileencodings:append({ "gbk", "cp936", "gb2312", "gb18030", "big5", "euc-jp", "euc-kr", "prc" })
+opt.termguicolors = true
+opt.completeopt = { "menu", "menuone", "noinsert" }
+opt.pumheight = 20
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+opt.sessionoptions:append({ "winpos", "globals", "skiprtp" })
+
+-- HACK: causes freezes on <= 0.9, so only enable on >= 0.10 for now
+opt.foldlevel = 99
+if vim.fn.has("nvim-0.10") == 1 then
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldexpr = "v:lua.require'util'.foldexpr()"
+  vim.opt.foldtext = ""
+  vim.opt.fillchars = "fold: "
+else
+  vim.opt.foldmethod = "indent"
+end
+
+if not vim.g.vscode then
+  vim.cmd.colorscheme("vscode")
+else
+  vim.notify = require("vscode-neovim").notify
+end
