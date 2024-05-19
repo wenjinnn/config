34cd22a3025efbda3904a8b2e9d4efbc7fbb310e
wenjin <hewenjin94@outlook.com>
2024-05-19 21:55:40 +0800

    fix(neovim): jdtls setup error

--- xdg/config/nvim/lua/lsp/jdtls/init.lua
+++ xdg/config/nvim/lua/lsp/jdtls/init.lua
@@ -37,7 +37,6 @@ function M.setup()
     require("jdtls.dap").setup_dap_main_class_configs()
     -- for all launch.json options see https://github.com/microsoft/vscode-java-debug#options
     require("dap.ext.vscode").load_launchjs()
-    lsp.setup(client, bufnr)
     lsp.set_keymap(
       bufnr,
       "n",
