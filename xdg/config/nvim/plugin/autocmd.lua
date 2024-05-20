d144dd920c3120a8a356a0a8cac042c6ba0c2e0c
wenjin <hewenjin94@outlook.com>
2024-05-20 15:40:42 +0800

    chore(neovim): adjust autocmd augroup

--- xdg/config/nvim/plugin/autocmd.lua
+++ xdg/config/nvim/plugin/autocmd.lua
@@ -1,7 +1,5 @@
--- This file is automatically loaded by lazyvim.config.init.
-
 local function augroup(name)
-  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("wenvim_" .. name, { clear = true })
 end
 
 -- Check if we need to reload the file when it changed
@@ -38,10 +36,10 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function(event)
     local exclude = { "gitcommit" }
     local buf = event.buf
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].wenvim_last_loc then
       return
     end
-    vim.b[buf].lazyvim_last_loc = true
+    vim.b[buf].wenvim_last_loc = true
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
