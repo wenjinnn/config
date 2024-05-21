0586a0f84bd4d858531a70d8b04b12068468a63a
wenjin <hewenjin94@outlook.com>
2024-05-21 21:50:52 +0800

    chore(nix): update flake.lock

--- flake.nix
+++ flake.nix
@@ -16,11 +16,9 @@
     ];
   };
 
-  inputs = let 
-      stable-version = "23.11";
-    in {
+  inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-${stable-version}";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -31,11 +29,11 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-${stable-version}";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-${stable-version}";
+      url = "github:nix-community/nix-on-droid/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ags.url = "github:Aylur/ags";
