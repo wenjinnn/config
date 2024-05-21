d9a6fe53997f66a31a7267626176f163111a08e1
wenjin <hewenjin94@outlook.com>
2024-05-21 10:11:23 +0800

    chore(neovim): adjust lualine filename config

--- xdg/config/nvim/lua/plugin/ui.lua
+++ xdg/config/nvim/lua/plugin/ui.lua
@@ -59,6 +59,7 @@ return {
       },
     },
     opts = function()
+      local filename_config = { "filename", path = 1 }
       return {
         options = {
           icons_enabled = true,
@@ -84,7 +85,7 @@ return {
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { { "filename", path = 1 } },
+          lualine_c = { filename_config },
           lualine_x = { "filesize", "encoding", "fileformat", "filetype" },
           lualine_y = { "progress" },
           lualine_z = { "location" },
@@ -92,7 +93,7 @@ return {
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = { "filename" },
+          lualine_c = { filename_config },
           lualine_x = { "location" },
           lualine_y = {},
           lualine_z = {},
