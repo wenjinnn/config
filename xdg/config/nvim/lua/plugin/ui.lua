885797c341624ebe24204bba7ea6184f410f8a91
wenjin <hewenjin94@outlook.com>
2024-05-21 17:10:35 +0800

    chore(neovim): add fix highlight pattern

--- xdg/config/nvim/lua/plugin/ui.lua
+++ xdg/config/nvim/lua/plugin/ui.lua
@@ -276,6 +276,7 @@ return {
       return {
         highlighters = {
           fixme = hi_words({ "FIXME", "Fixme", "fixme" }, "MiniHipatternsFixme"),
+          fix = hi_words({ "FIX", "Fix", "fix" }, "MiniHipatternsFixme"),
           hack = hi_words({ "HACK", "Hack", "hack" }, "MiniHipatternsHack"),
           todo = hi_words({ "TODO", "Todo", "todo" }, "MiniHipatternsTodo"),
           note = hi_words({ "NOTE", "Note", "note" }, "MiniHipatternsNote"),
