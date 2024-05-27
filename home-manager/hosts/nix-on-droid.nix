d206cb3f9e04ba7352acdef5173f7a00a2f02fcc
wenjin <hewenjin94@outlook.com>
2024-05-27 17:17:47 +0800

    feat(nix): add common overlays for nix-on-droid

--- home-manager/hosts/nix-on-droid.nix
+++ home-manager/hosts/nix-on-droid.nix
@@ -25,6 +25,31 @@
     # ./nvim.nix
   ];
 
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
   # TODO: Set your username
   home = {
     username = "nix-on-droid";
