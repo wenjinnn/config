cd2d117a356a8c04005f0ea3ae9339c9892e44bd
wenjin <hewenjin94@outlook.com>
2024-05-27 17:39:00 +0800

    feat(nix): add module for nix-on-droid

--- home-manager/hosts/nix-on-droid.nix
+++ home-manager/hosts/nix-on-droid.nix
@@ -10,13 +10,49 @@
   imports = with outputs.homeManagerModules; [
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
+    zsh
+    starship
+    neovim
+    git
+    ctags
+    yazi
+    mime
     git-sync
+    btop
+    lang
+    direnv
+    translate-shell
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
   ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
 
   # TODO: Set your username
   home = {
