0cdf34e52c79569cd0b0134d55b235578baf2503
wenjin <hewenjin94@outlook.com>
2024-05-27 18:04:25 +0800

    feat(nix): add git config for nix-on-droid

--- home-manager/hosts/nix-on-droid.nix
+++ home-manager/hosts/nix-on-droid.nix
@@ -20,6 +20,19 @@
     # ./nvim.nix
   ];
 
+  programs.git = {
+    enable = true;
+    userName = "wenjinnn";
+    userEmail = "hewenjin94@outlook.com";
+    extraConfig = {
+      color.ui = true;
+      credential.helper = "store";
+      github.user = "wenjinnn";
+      push.autoSetupRemote = true;
+      mergetool.keepBackup = false;
+    };
+  };
+
   # TODO: Set your username
   home = {
     username = "nix-on-droid";
