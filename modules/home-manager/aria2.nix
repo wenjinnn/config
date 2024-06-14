f400f49b7474e4c886e53a37cc571d48b24b7721
wenjin <hewenjin94@outlook.com>
2024-06-14 21:35:12 +0800

    chore(aria2): update bt-tracker

--- modules/home-manager/aria2.nix
+++ modules/home-manager/aria2.nix
@@ -6,7 +6,7 @@
     enable = true;
     # TODO get bt-tracker from https://github.com/ngosang/trackerslist, maybe write a script later.
     extraConfig = ''
-      bt-tracker=udp://93.158.213.92:1337/announce,udp://23.134.90.6:1337/announce,udp://185.243.218.213:80/announce,udp://208.83.20.20:6969/announce,udp://89.234.156.205:451/announce,udp://83.146.98.78:6969/announce,udp://35.231.115.55:1337/announce,udp://35.231.115.55:6969/announce,udp://207.241.226.111:6969/announce,udp://34.94.213.23:6969/announce,udp://61.216.149.33:6969/announce,udp://176.31.250.174:6969/announce,udp://82.156.24.219:6969/announce,udp://83.102.180.21:80/announce,
+      bt-tracker=http://93.158.213.92:1337/announce,udp://89.234.156.205:451/announce,udp://185.243.218.213:80/announce,udp://23.153.248.83:6969/announce,udp://167.99.185.219:6969/announce,udp://208.83.20.20:6969/announce,udp://135.125.202.143:6969/announce,udp://176.56.6.248:6969/announce,udp://176.31.250.174:6969/announce,udp://83.102.180.21:80/announce,udp://37.235.176.37:2710/announce,udp://45.9.60.30:6969/announce,udp://37.27.4.53:6969/announce,udp://94.243.222.100:6969/announce,udp://185.216.179.62:25/announce,udp://23.157.120.14:6969/announce
     '';
     settings = {
       listen-port = 4001;
