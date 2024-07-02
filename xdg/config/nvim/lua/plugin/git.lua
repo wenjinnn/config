82f04153c21f0697e3d2050e78bfff04f54d18d0
wenjin <hewenjin94@outlook.com>
2024-07-02 11:38:32 +0800

    feat(neovim): setup numhl for gitsigns, also disable signcolumn

--- xdg/config/nvim/lua/plugin/git.lua
+++ xdg/config/nvim/lua/plugin/git.lua
@@ -98,6 +98,8 @@ return {
     },
     config = function()
       require("gitsigns").setup({
+        signcolumn = false,
+        numhl = true,
         current_line_blame = true,
         current_line_blame_opts = {
           delay = 500,
