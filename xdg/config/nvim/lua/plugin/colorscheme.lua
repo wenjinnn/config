6e8414bf40a61c348051362d58835bcff5efaa69
wenjin <hewenjin94@outlook.com>
2024-07-04 10:15:07 +0800

    feat!(neovim): switch colorscheme to tokyonight

--- xdg/config/nvim/lua/plugin/colorscheme.lua
+++ xdg/config/nvim/lua/plugin/colorscheme.lua
@@ -1,31 +1,19 @@
 local not_vscode = require("util").not_vscode
 return {
   {
-    "Mofiqul/vscode.nvim",
+    "folke/tokyonight.nvim",
     cond = not_vscode,
+    lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme("vscode")
+      vim.cmd.colorscheme("tokyonight")
     end,
-    opts = function()
-      local c = require("vscode.colors").get_colors()
-      return {
-        transparent = true,
-        terminal_colors = false,
-        group_overrides = {
-          -- this supports the same val table as vim.api.nvim_set_hl
-          -- use colors from this colorscheme by requiring vscode.colors!
-          ["MiniPickMatchCurrent"] = { link = "PmenuSel" },
-          ["@interface"] = { link = "@constant" },
-          ["@lsp.type.interface"] = { link = "@interface" },
-          ["@org.hyperlink"] = { link = "Underlined" },
-          CmpItemMenu = { link = "Comment" },
-          -- vim-dadbod-ui
-          NotificationInfo = { link = "DiagnosticFloatingInfo" },
-          NotificationError = { link = "DiagnosticFloatingError" },
-          NotificationWarning = { link = "DiagnosticFloatingWarning" },
-        },
-      }
-    end,
+    opts = {
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = false,
+      on_colors = function(colors)
+        colors.bg_statusline = colors.none
+      end,
+    },
   },
 }
