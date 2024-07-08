ba4dda7eba5d0a0eff6c9493acfa7ea1736f00f9
wenjin <hewenjin94@outlook.com>
2024-07-08 15:34:12 +0800

    feat(tmux): configuretmux with home manager

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,11 +1,66 @@
 {
+  pkgs,
+  ...
+}: {
   programs.tmux = {
     enable = true;
-  };
-  home.file = {
-    ".config/tmux" = {
-      source = ../../xdg/config/tmux;
-      recursive = true;
-    };
+    prefix = "C-q";
+    aggressiveResize = true;
+    clock24 = true;
+    customPaneNavigationAndResize = true;
+    escapeTime = 0;
+    historyLimit = 10000;
+    baseIndex = 1;
+    mouse = true;
+    keyMode = "vi";
+    terminal = "tmux-256color";
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_left_separator "█"
+          set -g @catppuccin_window_middle_separator "█"
+          set -g @catppuccin_window_right_separator "█"
+          set -g @catppuccin_window_status_enable "yes"
+          set -g @catppuccin_window_status_icon_enable "yes"
+          set -g @catppuccin_window_default_text "#W"
+          set -g @catppuccin_window_current_text "#W"
+          set -g @catppuccin_status_modules_right "application directory up_time date_time user host"
+          set -g @catppuccin_status_modules_left "session"
+          set -g @catppuccin_status_left_separator "█"
+          set -g @catppuccin_status_right_separator "█"
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '15'
+        '';
+      }
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
+      open
+      cpu
+      yank
+      sensible
+      logging
+      sessionist
+      pain-control
+    ];
+    extraConfig = ''
+      # True color support
+      set -ag terminal-overrides ",$TERM:RGB"
+      # Undercurl support
+      set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+      # Underscore colors
+      set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+      '';
   };
 }
