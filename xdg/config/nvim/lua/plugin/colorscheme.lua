98a428778f98dc1b763b72f0c084a0cab2a9933d
wenjin <hewenjin94@outlook.com>
2024-07-08 15:33:28 +0800

    feat(neovim): use catppuccin & update lazy-lock.json

--- xdg/config/nvim/lua/plugin/colorscheme.lua
+++ xdg/config/nvim/lua/plugin/colorscheme.lua
@@ -1,31 +1,25 @@
 local not_vscode = require("util").not_vscode
 return {
   {
-    "Mofiqul/vscode.nvim",
+    "catppuccin/nvim",
     cond = not_vscode,
+    name = "catppuccin",
     priority = 1000,
-    init = function()
-      vim.cmd.colorscheme("vscode")
-    end,
-    opts = function()
-      local c = require("vscode.colors").get_colors()
-      return {
-        transparent = true,
-        terminal_colors = false,
-        group_overrides = {
-          -- this supports the same val table as vim.api.nvim_set_hl
-          -- use colors from this colorscheme by requiring vscode.colors!
-          ["MiniPickMatchCurrent"] = { link = "PmenuSel" },
-          ["@interface"] = { link = "@constant" },
-          ["@lsp.type.interface"] = { link = "@interface" },
-          ["@org.hyperlink"] = { link = "Underlined" },
-          CmpItemMenu = { link = "Comment" },
-          -- vim-dadbod-ui
-          NotificationInfo = { link = "DiagnosticFloatingInfo" },
-          NotificationError = { link = "DiagnosticFloatingError" },
-          NotificationWarning = { link = "DiagnosticFloatingWarning" },
+    opts = {
+      integrations = {
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
         },
-      }
+      },
+    },
+    init = function()
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
