c990dff50b99864f73e0276eb99879db4594bb3f
wenjin <hewenjin94@outlook.com>
2024-07-23 11:49:39 +0800

    chore(neovim): remove terminal esc keymap

--- xdg/config/nvim/plugin/keymaps.lua
+++ xdg/config/nvim/plugin/keymaps.lua
@@ -32,8 +32,6 @@ map("n", "<leader>C", "<cmd>!ctags<cr>", { desc = "Ctags" })
 
 map("n", "<leader>F", "<cmd>lua require'util'.maximize()<cr>", { desc = "Maximize current buffer" })
 
-map("t", "<Esc>", "<C-\\><C-n>")
-
 if vim.g.vscode then
   local action = function(action, opts)
     return function()
