017ab95728550d5fa9f4f5d69615adcaa1ff81bc
wenjin <hewenjin94@outlook.com>
2024-07-24 19:53:11 +0800

    chore(neovim): improve treesitter config

--- xdg/config/nvim/lua/plugin/editing.lua
+++ xdg/config/nvim/lua/plugin/editing.lua
@@ -184,10 +184,7 @@ return {
       { "nvim-treesitter/nvim-treesitter-textobjects" },
       { "nvim-treesitter/nvim-treesitter-context" },
       { "windwp/nvim-ts-autotag", opts = {} },
-      {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        opts = { enable_autocmd = false },
-      },
+      { "JoosepAlviste/nvim-ts-context-commentstring", opts = { enable_autocmd = false } },
       { "hiphish/rainbow-delimiters.nvim" },
     },
     opts = {
@@ -232,19 +229,16 @@ return {
         additional_vim_regex_highlighting = false,
       },
       indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     },
-    config = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        local added = {}
-        opts.ensure_installed = vim.tbl_filter(function(lang)
-          if added[lang] then
-            return false
-          end
-          added[lang] = true
-          return true
-        end, opts.ensure_installed)
-      end
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+    main = "nvim-treesitter.configs",
   },
 }
