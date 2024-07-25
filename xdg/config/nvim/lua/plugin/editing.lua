45bc0da33635a8c4a808acf68c390e95f9b27eae
wenjin <hewenjin94@outlook.com>
2024-07-25 09:32:52 +0800

    feat(neovim): improve mini.splitjoin setting

--- xdg/config/nvim/lua/plugin/editing.lua
+++ xdg/config/nvim/lua/plugin/editing.lua
@@ -109,7 +109,22 @@ return {
   {
     "echasnovski/mini.splitjoin",
     event = "BufRead",
-    opts = {},
+    opts = function()
+      local gen_hook = require("mini.splitjoin").gen_hook
+      local curly = { brackets = { '%b{}' } }
+      -- Add trailing comma when splitting inside curly brackets
+      local add_comma_curly = gen_hook.add_trailing_separator(curly)
+
+      -- Delete trailing comma when joining inside curly brackets
+      local del_comma_curly = gen_hook.del_trailing_separator(curly)
+
+      -- Pad curly brackets with single space after join
+      local pad_curly = gen_hook.pad_brackets(curly)
+      return {
+        split = { hooks_post = { add_comma_curly } },
+        join = { hooks_post = { del_comma_curly, pad_curly } },
+      }
+    end,
   },
   {
     "echasnovski/mini.ai",
