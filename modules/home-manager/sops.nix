9485ebbad9e79e7afb40282c45aa4aa3d145adfd
wenjin <hewenjin94@outlook.com>
2024-07-29 19:59:04 +0800

    chore(nix): adjust sops setting

--- modules/home-manager/sops.nix
+++ modules/home-manager/sops.nix
@@ -9,11 +9,16 @@
   ];
 
   sops = {
-    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt"; # must have no password!
     defaultSopsFile = ../../secrets.yaml;
+    age = {
+      generateKey = true;
+      sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+      keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt"; # must have no password!
+    };
   };
 
   home.packages = with pkgs; [
+    ssh-to-age
     age
     sops
   ];
