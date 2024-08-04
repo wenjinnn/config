38da5932441fde13b893626082791f74b6bde96f
wenjin <hewenjin94@outlook.com>
2024-08-04 14:05:09 +0800

    feat(neovim): add nixd options config

--- xdg/config/nvim/lua/lsp/nixd/init.lua
+++ xdg/config/nvim/lua/lsp/nixd/init.lua
@@ -1,8 +1,20 @@
+local hostname = vim.fn.hostname()
+local username = os.getenv("USER")
 return {
   settings = {
     nixd = {
       formatting = {
         command = { "alejandra", "--" },
+      },
+      options = {
+        nixos = {
+          expr = string.format('(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.%s.options',
+            hostname),
+        },
+        home_manager = {
+          expr = string.format('(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."%s@%s".options',
+            username, hostname),
+        },
       },
     },
   },
