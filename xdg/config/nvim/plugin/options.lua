6f940f18534135fe927f02ddfb40556bc0785598
wenjin <hewenjin94@outlook.com>
2024-08-09 15:25:21 +0800

    chore(neovim): adjust completeopt

--- xdg/config/nvim/plugin/options.lua
+++ xdg/config/nvim/plugin/options.lua
@@ -73,7 +73,7 @@ opt.confirm = true
 opt.updatetime = 500
 opt.fileencodings:append({ "gbk", "cp936", "gb2312", "gb18030", "big5", "euc-jp", "euc-kr", "prc" })
 opt.termguicolors = true
-opt.completeopt = { "menu", "menuone", "noinsert" }
+opt.completeopt:append { "menuone", "noinsert" }
 opt.pumheight = 20
 opt.shortmess:append({ I = true })
 opt.sessionoptions:append({ "winpos", "globals", "skiprtp" })
