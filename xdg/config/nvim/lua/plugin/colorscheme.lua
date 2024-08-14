4dbdc6be2f8b332c8bccad26d6678c7dca0dd9ac
wenjin <hewenjin94@outlook.com>
2024-08-14 19:51:53 +0800

    feat(neovim): use mini.hues instead of catppuccin

--- xdg/config/nvim/lua/plugin/colorscheme.lua
+++ xdg/config/nvim/lua/plugin/colorscheme.lua
@@ -2,21 +2,17 @@ local in_vscode = require("util").in_vscode
 if in_vscode() then
   return
 end
-local add, now = MiniDeps.add, MiniDeps.now
+local now = MiniDeps.now
 now(function()
-  add({ source = "catppuccin/nvim" })
-  require("catppuccin").setup({
-    integrations = {
-      native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-      },
+  require("mini.colors").setup()
+  require("mini.hues").setup({
+    background = "#1e2131",
+    foreground = "#c4c6cd",
   })
-  vim.cmd.colorscheme("catppuccin")
+  vim.api.nvim_set_hl(0, "@lsp.type.interface", { link = "@interface" })
+  vim.api.nvim_set_hl(0, "@interface", { link = "@constant" })
+  local visual = vim.api.nvim_get_hl(0, { name = "Visual" })
+  visual.bold = true
+  ---@diagnostic disable-next-line: param-type-mismatch
+  vim.api.nvim_set_hl(0, "Visual", visual)
 end)
