6d3c024aa9042bba2927f96b8bea13ae21f61d28
wenjin <hewenjin94@outlook.com>
2024-08-15 09:55:00 +0800

    feat(neovim): make comment and diagnostic italicized

--- xdg/config/nvim/colors/wenvim-blue.lua
+++ xdg/config/nvim/colors/wenvim-blue.lua
@@ -1,10 +1,4 @@
-vim.api.nvim_set_hl(0, "@lsp.type.interface", { link = "@interface" })
-vim.api.nvim_set_hl(0, "@interface", { link = "@constant" })
-local visual = vim.api.nvim_get_hl(0, { name = "Visual" })
-visual.bold = true
----@diagnostic disable-next-line: param-type-mismatch
-vim.api.nvim_set_hl(0, "Visual", visual)
-
+---@diagnostic disable: param-type-mismatch
 local base_color = {
   background = "#1e2131",
   foreground = "#c4c6cd",
@@ -19,4 +13,20 @@ vim.g.terminal_color_12 = palette.azure_mid2
 vim.g.terminal_color_13 = palette.purple_mid2
 vim.g.terminal_color_14 = palette.cyan_mid2
 vim.g.terminal_color_15 = palette.fg_mid2
+vim.api.nvim_set_hl(0, "@lsp.type.interface", { link = "@interface" })
+vim.api.nvim_set_hl(0, "@interface", { link = "@constant" })
+
+local function override_hl(name, opts)
+  local hl = vim.api.nvim_get_hl(0, { name = name })
+  hl = vim.tbl_deep_extend("force", hl, opts)
+  vim.api.nvim_set_hl(0, name, hl)
+end
+override_hl("Visual", { bold = true })
+override_hl("Comment", { italic = true })
+override_hl("DiagnosticError", { italic = true })
+override_hl("DiagnosticWarn", { italic = true })
+override_hl("DiagnosticInfo", { italic = true })
+override_hl("DiagnosticHint", { italic = true })
+override_hl("DiagnosticOk", { italic = true })
+
 vim.g.colors_name = "wenvim-blue"
