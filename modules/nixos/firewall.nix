b92a763561b98d37f6d2c00b8613f8629adde917
wenjin <hewenjin94@outlook.com>
2024-10-09 18:02:08 +0800

    chore(firewall): add allowed tcp port

--- modules/nixos/firewall.nix
+++ modules/nixos/firewall.nix
@@ -4,7 +4,7 @@
     enable = true;
     checkReversePath = "loose";
     trustedInterfaces = ["tun*" "Meta"];
-    allowedTCPPorts = [80 443 4001];
+    allowedTCPPorts = [80 443 4001 8080];
     allowedUDPPortRanges = [
       {
         from = 4000;
