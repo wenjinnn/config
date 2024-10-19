10ecb9e4e244517fc15d6fb1a58394c0df853813
wenjin <hewenjin94@outlook.com>
2024-10-19 11:04:38 +0800

    feat(nix): adjust text mime app to nvim

--- modules/home-manager/mime.nix
+++ modules/home-manager/mime.nix
@@ -3,7 +3,7 @@ let
   file-manager = "org.gnome.Nautilus.desktop";
   image-viewer = "org.gnome.Loupe.desktop";
   mail = "org.gnome.Evolution.desktop";
-  code = "code.desktop";
+  nvim = "nvim.desktop";
 in {
   xdg.mimeApps = rec {
     enable = true;
@@ -16,7 +16,7 @@ in {
       "x-scheme-handler/mailto" = mail;
       "application/xhtml+xml" = browser;
       "text/html" = browser;
-      "text/plain" = code;
+      "text/plain" = nvim;
 
       "application/pdf" = browser;
 
