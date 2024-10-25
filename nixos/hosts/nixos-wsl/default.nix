a8950ee3c608eaaef2c4a36290c3445969bfbe6a
wenjin <hewenjin94@outlook.com>
2024-10-25 22:01:38 +0800

    feat(wsl): move wsl module to host config

--- nixos/hosts/nixos-wsl/default.nix
+++ nixos/hosts/nixos-wsl/default.nix
@@ -2,6 +2,7 @@
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
   inputs,
+  username,
   outputs,
   lib,
   config,
@@ -11,7 +12,7 @@
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
-    outputs.nixosModules.wsl
+    inputs.nixos-wsl.nixosModules.default
 
     # Or modules from other flakes (such as nixos-hardware):
     # inputs.hardware.nixosModules.common-cpu-amd
@@ -27,8 +28,17 @@
     ];
   };
 
+  services.ollama.acceleration = "cuda";
+
   networking = {
     firewall.enable = false;
     hostName = "nixos-wsl";
+  };
+
+  wsl = {
+    enable = true;
+    defaultUser = "${username}";
+    startMenuLaunchers = true;
+    useWindowsDriver = true;
   };
 }
