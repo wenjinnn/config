e259fbef6aea8f126a4091eb242de758e14e1d8c
wenjin <hewenjin94@outlook.com>
2024-11-29 14:05:08 +0800

    feat(mail): remove markdown highlight

--- modules/home-manager/mail.nix
+++ modules/home-manager/mail.nix
@@ -225,25 +225,6 @@ in {
 
         # Credit
         color body brightwhite default "(signed-off|co-authored)-by: .*"
-
-        # Markdown highlight, copied from https://github.com/neomutt/dyk/issues/12
-        # Title: # Text
-        color body brightcyan   default     '^#\+ .*'
-
-        # Bullet points, or numbered lists
-        color body brightred    default     '^ *[-*+] '
-        color body brightred    default     '^ *[0-9]+\. '
-
-        # Emphasis: _word_, *word*, /word/
-        color body brightyellow default     '\<_\S+_\>'
-        color body brightyellow default     '\*\<\S+\>\*'
-        color body brightyellow default     '/\<\S+\>/'
-
-        # Code block: `text`
-        color body brightwhite  brightblack '`[^`]+`'
-
-        # GitHub nick: (@flatcap)
-        color body green        default     '(@[-a-zA-Z0-9_]+)'
       '';
     };
     notmuch.enable = true;
