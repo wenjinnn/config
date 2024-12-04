b9f7a12b9a655cd7358e7b80cd1e9b20a085b5c0
wenjin <hewenjin94@outlook.com>
2024-12-04 23:40:45 +0800

    feat(mpd): set aodio output type to pipewire
    also format some code

--- modules/home-manager/mpd.nix
+++ modules/home-manager/mpd.nix
@@ -4,15 +4,35 @@
   ];
   services = {
     mpd-mpris.enable = true;
-    mpd.enable = true;
+    mpd = {
+      enable = true;
+      extraConfig = ''
+        audio_output {
+          type  "pipewire"
+          name  "PipeWire Sound Server"
+        }
+      '';
+    };
   };
   programs.ncmpcpp = {
     enable = true;
     bindings = [
-      { key = "j"; command = "scroll_down"; }
-  { key = "k"; command = "scroll_up"; }
-  { key = "J"; command = [ "select_item" "scroll_down" ]; }
-  { key = "K"; command = [ "select_item" "scroll_up" ]; }
-  ];
+      {
+        key = "j";
+        command = "scroll_down";
+      }
+      {
+        key = "k";
+        command = "scroll_up";
+      }
+      {
+        key = "J";
+        command = ["select_item" "scroll_down"];
+      }
+      {
+        key = "K";
+        command = ["select_item" "scroll_up"];
+      }
+    ];
   };
 }
